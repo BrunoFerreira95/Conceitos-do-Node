706f7fd4884d29c4b2e95152f2e3a93e6bee39f9
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2020-04-07 18:05:53 -0300

    Creating middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,31 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository ID.'})
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+// app.use('/repositories/:id', validateProjectId);
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -30,7 +55,7 @@ app.post("/repositories", (request, response) => {
   return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", validateProjectId, (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
@@ -55,7 +80,7 @@ app.put("/repositories/:id", (request, response) => {
   return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", validateProjectId, (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex( repository => repository.id === id);
@@ -69,7 +94,7 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", validateProjectId, (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex( repository => repository.id === id);
